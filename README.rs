cb7821de975193cc1abeeca134f755fa0bf3f491
Raymond <rjrosales2025@gmail.com>
2023-11-14 16:39:13 -0500

    made README look nice

--- README.rs
+++ README.rs
@@ -1,7 +1,24 @@
-// CLONCK
-//
-// To build:
-// $ cargo build
-//
-// To run:
-// $ cargo run clonck
+/*******************************************************************************
+
+
+              mmmm   mmmm                                    mm       
+            ##""""#  ""##                                    ##       
+           ##"         ##       m####m   ##m####m   m#####m  ## m##"  
+           ##          ##      ##"  "##  ##"   ##  ##"    "  ##m##    
+           ##m         ##      ##    ##  ##    ##  ##        ##"##m   
+            ##mmmm#    ##mmm   "##mm##"  ##    ##  "##mmmm#  ##  "#m  
+              """"      """"     """"    ""    ""    """""   ""   """ 
+                     A clock that isn't dumb and stupid
+
+
+INSTALLATION:
+
+Clone the repo and cd into it.
+To build, type the following in your prompt:
+  $ cargo build
+To run, type the following in your repo:
+  $ cargo run clonck
+
+
+
+*******************************************************************************/

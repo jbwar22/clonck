b2a90aad7fcd07d1618e589aa4823e873395d998
Raymond <rjrosales2025@gmail.com>
2023-11-14 16:46:52 -0500

    added FAQ

--- README.rs
+++ README.rs
@@ -20,5 +20,19 @@ To run, type the following in your repo:
   $ cargo run clonck
 
 
+FAQ:
+
+Q - Why is the README not a Markdown file?
+A - So that this project is ONE HUNDRED PERCENT Rust.
+
+Q - What's the point of this project?
+A - Clonck aims to be fast, accurate, and Japanese. It accomplishes all three of
+    these goals in spades. To be more specific, this program was intended to be
+    used in Linux status bars like Waybar. But obviously, it can be used however
+    you'd like.
+
+Q - Why "Clonck"?
+A - Why not?
+
 
 *******************************************************************************/
